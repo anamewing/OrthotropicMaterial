55e2c72f4a6c3e4459d3188e9c0d52c14fe4585c
mewing <anamewing@gmail.com>
2012-10-14 04:09:12 +0800

    修正转动bug
    Signed-off-by: mewing <anamewing@gmail.com>

--- getMainDirectionStress.m
+++ getMainDirectionStress.m
@@ -2,7 +2,7 @@ function news=getMainDirectionStress(stress,theta)
 %逆时针转动theta得到主应力
 T=getRotateMatrix(theta);
 ssvic=[stress.sx,stress.sy,stress.sxy].';
-nssvic=T*ssvic;
+nssvic=inv(T)*ssvic;
 news.sx=nssvic(1);
 news.sy=nssvic(2);
 news.sxy=nssvic(3);
